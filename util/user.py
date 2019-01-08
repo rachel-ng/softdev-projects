38e5452337861bfbb95b28abcd5b027f8798f9fc
tina <tinaw425@gmail.com>
2019-01-08 10:57:48 -0500

    added register/login

--- util/user.py
+++ util/user.py
@@ -1,4 +1,4 @@
-#code to create user table in database.db
+#code to create user table in health.db
 import sqlite3
 from passlib.hash import sha256_crypt
 
@@ -15,11 +15,36 @@ def create_table():
     c.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, user TEXT, password TEXT)")
     c.execute("CREATE TABLE IF NOT EXISTS basic_info (user_id INTEGER, height REAL, weight REAL)")
     c.execute("CREATE TABLE IF NOT EXISTS water (user_id INTEGER, current_intake REAL, expected_intake REAL)")
-    c.execute("CREATE TABLE IF NOT EXISTS sleep (user_id INTEGER, current_intake REAL, expected_intake REAL)")
     c.execute("CREATE TABLE IF NOT EXISTS diet (user_id INTEGER, allergies TEXT, carbs REAL, protein REAL, fat REAL)")
     c.execute("CREATE TABLE IF NOT EXISTS expected_diet (user_id INTEGER, expected_carbs REAL, expected_protein REAL, expected_fat REAL)")
 
     db.commit() #save changes
     db.close()
 
+def register(username, password):
+    '''This function adds the user to the database.
+    If username already exists, returns false. Otherwise, the function inserts a row in users and returns true.'''
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    data = c.execute("SELECT * FROM users;")
+    for row in data:
+        if username == row[1]:
+            return False
+    params = (username, sha256_crypt.hash(password))
+    c.execute("INSERT INTO users (user, password) VALUES (?, ?)", params)
+    db.commit()
+    db.close()
+    return True
+
+def authenticate(username, password):
+    '''This function checks user login. If username and encrypted password match, the function returns true. The function returns false otherwise.'''
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    data = c.execute("SELECT * FROM users")
+    for row in data:
+        if row[1] == username and sha256_crypt.verify(password, row[2]):
+            db.close()
+            return True
+    db.close()
+    return False
 # create_table()
