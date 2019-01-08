d486d1f418a22e24cda0a33de9386c20aebd2261
tina <tinaw425@gmail.com>
2019-01-08 11:09:06 -0500

    update

--- app.py
+++ app.py
@@ -1,17 +1,29 @@
-from flask import Flask, render_template, request, session, url_for, redirect, flash
+import os
+
+from flask import Flask, render_template, request, session, redirect, url_for, flash
+
+from util import user
 
 import os
 app = Flask(__name__) #create instance of class flask
 
-# app.secret_key = os.urandom(32)
+app.secret_key = os.urandom(32)
 
 
-app.route("/", methods = ["GET", "POST"])
+@app.route("/", methods = ["GET", "POST"])
 def index():
-    print("Cowabunga!")
-    return "No hablo queso!"
+    return render_template("index.html")
 
+@app.route('/signup')
+def signup():
+    '''This function renders the HTML template for the signup page.'''
+    return render_template('signup.html')
 
+@app.route('/login')
+def login():
+    '''This function renders the HTML template for the login page.'''
+    return render_template('login.html')
+    
 if __name__ == "__main__":
     app.debug = True
     app.run()
