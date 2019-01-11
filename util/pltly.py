093417b0ec3eb52b79dcfe1fd86a284ec7403913
rng <rng@stuy.edu>
2019-01-11 12:43:58 -0500

    plotly pls

---
+++ util/pltly.py
@@ -0,0 +1,17 @@
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+trace0 = go.Bar(
+    x=[1, 2, 3, 4],
+    y=[10, 15, 13, 17]
+)
+trace1 = go.Bar(
+    x=[1, 2, 3, 4],
+    y=[16, 5, 11, 9]
+)
+data = [trace0, trace1]
+
+new_chart = plotly.offline.plot(data, output_type='div')
+
+print(new_chart)
