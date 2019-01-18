4e285ca12ceab0cb9e6eada89e28aa2f671021a1
rng <rng@stuy.edu>
2019-01-17 19:00:28 -0500

    more charts stuff, updated templates for pages

--- plotly_tests.py
+++ plotly_tests.py
@@ -14,32 +14,22 @@ def plotly_tests():
     username = 'r'
 
     sleep_data = chart_data.get_user_sleep(username)
-
     plotly_charts.sleep_chart(sleep_data, "sleep")
 
-    user_macros = [food.get_total_carbs(username), food.get_total_protein(username), food.get_total_fat(username)]
+    water_data = chart_data.get_user_water(username)
+    plotly_charts.bar_chart(water_data, "water", "rgb(88,180,197)")
 
+    user_macros = [food.get_total_carbs(username), food.get_total_protein(username), food.get_total_fat(username)]
     plotly_charts.macros_chart(user_macros, "macros")
 
     food_data = chart_data.get_user_food(username)
-
-    print (food_data)
-
     plotly_charts.calorie_chart(food_data, "calories")
 
-    water_data = chart_data.get_user_water(username)
-
-    plotly_charts.bar_chart(water_data, "water")
-
     exercise_data = chart_data.get_user_exercise(username)
-
-    print(exercise_data)
-
-    plotly_charts.bar_chart(exercise_data, "exercise")
+    plotly_charts.bar_chart(exercise_data, "exercise", "rgb(224,75,101)")
 
     return render_template('all_charts.html')
 
 if __name__ == "__main__":
     app.debug = True
     app.run()
-
